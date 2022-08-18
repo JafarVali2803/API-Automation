424f0be46e6ff4338007b0b6e158571bae551055
Jafar Vali Juturu <jafarj@testvagrant.com>
2022-08-18 17:37:55 +0530

    get users test modified

--- src/test/java/Sample.java
+++ src/test/java/Sample.java
@@ -1,3 +1,4 @@
+import io.restassured.http.ContentType;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -18,4 +19,24 @@ public class Sample {
                         .statusCode(200)
                         .log().body();
     }
+   @Test
+   public void shouldCreateUser(){
+        given()
+               .accept(ContentType.JSON)
+               .contentType(ContentType.JSON)
+               .header("Authorization", "Bearer e5efd1c0e45273b0369c22a479a2c8ef6335343001d046c4bcc63e6d22a27b67")
+               .body("{\n" +
+                       "    \"name\": \"Tenawerdbnfxgldi Ramakrishna\",\n" +
+                       "    \"gender\": \"male\",\n" +
+                       "    \"email\": \"tenhgcgjghali.ramakrifshnda@15ce.com\",\n" +
+                       "    \"status\": \"active\"\n" +
+                       "}")
+               .when()
+                   .post("https://gorest.co.in/public/v2/users")
+
+                .then()
+                   .statusCode(201)
+                   .log().body();
+   }
+
 }
