5541c9604037d611e2189f1a6012a351465d7f6d
Jafar Vali Juturu <jafarj@testvagrant.com>
2022-08-18 16:39:34 +0530

    get users test

--- src/test/java/Sample.java
+++ src/test/java/Sample.java
@@ -1,9 +1,21 @@
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import static io.restassured.RestAssured.given;
+
 public class Sample {
     @Test
-    public void sampleTest() {
-        Assert.assertEquals(1,1);
+    public void shouldGetAllUsers() {
+
+        //1. Arrange - Creates the data required to run the tests
+        //2. Act - Actions we perform
+        //3. Assert - Will compare actual and expected values.
+        //BDD syntax
+        given()
+                .when()
+                      .get("https://gorest.co.in/public/v2/users")
+                .then()
+                        .statusCode(200)
+                        .log().body();
     }
 }
